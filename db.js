ce401a23808a0927145c66294134efd8d5efedd9
Siddharth Shyniben <siddhart.muscat@gmail.com>
2023-07-05 15:01:27 +0530

    add auth

---
+++ db.js
@@ -0,0 +1,6 @@
+export function createUser () {}
+export function userExists () {}
+export function getUser () {}
+export function storeRefreshToken () {}
+export function refreshTokenExists () {}
+export function invalidateRefreshToken () {}
--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-import express from 'express';
-import morgan from 'morgan';
 import { dirname, join } from 'path';
-import genCode from './tutorial-parser.js';
 import { existsSync } from 'fs';
 import { fileURLToPath } from 'url';
+
+import genCode from './tutorial-parser.js';
+import {createUser, userExists, getUser, storeRefreshToken, refreshTokenExists, invalidateRefreshToken} from './db.js';
+
+import express from 'express';
+import morgan from 'morgan';
+import 'dotenv/config';
+import Aaauth from 'aaauth';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const PORT = 3000;
 
-let generatedCode;
+const aaauth = Aaauth({
+	jwtSecret: process.env.JWT_SECRET,
+	createUser, userExists, getUser, storeRefreshToken, refreshTokenExists, invalidateRefreshToken
+});
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(morgan('tiny'))
 app.use(express.static(join(__dirname, 'public')))
+app.use(aaauth.router);
 
 app.get('/', (_req, res) => {
 	res.send('<a href="/minimax">Minimax</a>');
 });
 
-app.get('/:tutorial', async (req, res) => {
+let generatedCode;
+
+app.get('/:tutorial', aaauth.authenticate, async (req, res) => {
 	const filePath = join(__dirname, 'tutorials', `${req.params.tutorial}.hjson`)
 
 	if (!existsSync(filePath)) {
